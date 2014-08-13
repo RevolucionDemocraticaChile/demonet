614d650c7e77ce1434c87fc83eb1fbde3fb6a1bb
Gonzalo Diaz C <gdiazc@cs.ox.ac.uk>
2014-08-13 14:54:43 +0100

    minimium cost BCrypt for testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Speed up tests by lowering bcrypt's cost function.
+  ActiveModel::SecurePassword.min_cost = true
 end

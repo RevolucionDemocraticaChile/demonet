b55ab0aa594afbbdfaa7aad7fcbec7386ae1eecb
Gonzalo Diaz C <gdiazc@cs.ox.ac.uk>
2014-08-15 18:23:41 +0100

    ui updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,15 +53,22 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      flash[:success] = 'Usuario fue actualizado exitosamente.'
+      redirect_to @user
+    else
+      render :edit
     end
+
+    # respond_to do |format|
+    #   if @user.update(user_params)
+    #     format.html { redirect_to @user, notice: 'User was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @user }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /users/1

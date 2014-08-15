a45b263bf00b6c815b8833e635d41f748cbbfb8f
Gonzalo Diaz C <gdiazc@cs.ox.ac.uk>
2014-08-15 20:27:15 +0100

    ui changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 module UsersHelper
 
-	def gravatar_for(user)
+	def gravatar_for(user, size = 100)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
 		image_tag(gravatar_url, alt: user.name, class: "gravatar img-circle")
+	end
+
+	def actas_recientes_count_for(user)
+		user.actas.where(date: (DateTime.now-6.months)..(DateTime.now)).count
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :territorios
-  resources :member_territorios, only: [:create, :destroy]
+  resources :member_territorios, only: [:new, :create, :destroy]
   resources :actas
   resources :user_actas
 

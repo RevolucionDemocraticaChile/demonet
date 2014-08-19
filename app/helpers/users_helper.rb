2dd456ed751c8aebe857048507e78c5d539a6357
jjatria <jjatria@gmail.com>
2014-08-19 15:25:12 -0400

    Fixed broken avatar URL

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,12 @@
 module UsersHelper
 
   GRAVATAR_DEFAULT_SIZE = 100
-  LOGO_RD_SMALL_URL = "https://github.com/RevolucionDemocraticaChile/demonet/blob/development/app/assets/images/rd_faces.bw-trans.png"
+  LOGO_RD_SMALL_URL = "https://raw.githubusercontent.com/RevolucionDemocraticaChile/demonet/development/app/assets/images/Logo_RD_small.png"
+  RD_FACES_BW_URL = "https://raw.githubusercontent.com/RevolucionDemocraticaChile/demonet/development/app/assets/images/rd_faces.bw-trans.png"
 
   def gravatar_for(user, size = GRAVATAR_DEFAULT_SIZE)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{u(LOGO_RD_SMALL_URL)}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{u(RD_FACES_BW_URL)}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar img-circle", size: "#{GRAVATAR_DEFAULT_SIZE}")
   end
 

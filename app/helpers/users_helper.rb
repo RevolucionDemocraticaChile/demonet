fe7e6fedc06e770119e3963ef29c364130f84add
Gonzalo Diaz C <gdiazc@cs.ox.ac.uk>
2014-08-19 19:09:23 +0100

    added gravatar_default_size constant and specified width in img tag for cleaner loading

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
 
-  def gravatar_for(user, size = 100)
+  GRAVATAR_DEFAULT_SIZE = 100
+
+  def gravatar_for(user, size = GRAVATAR_DEFAULT_SIZE)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar img-circle")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar img-circle", size: "#{GRAVATAR_DEFAULT_SIZE}")
   end
 
   def recent_attendance_for(user)

e691d9daecef0f4d1c2b74e2c7ff5dc29c9d7d4f
Gonzalo Diaz C <gdiazc@cs.ox.ac.uk>
2014-08-21 19:34:27 +0100

    small fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def format_meeting_name(meeting)
-    "#{t(:meeting).humanize} #{(meeting.groups.map { |g| g.name }).join(',')} #{l(Date.parse(meeting.date.to_s))}"
+    "#{t(:meeting).humanize} #{(meeting.groups.map { |g| g.name }).join(', ')} #{l(Date.parse(meeting.date.to_s))}"
   end
 
   # def format_group_names(groups)

f2da5254c2c42cb29c9b2ccbd5c12765b1da612e
rodrihub <saigo.getsuga@gmail.com>
2015-01-15 21:08:17 +0000

    Added responsive table to user table and filter in groups columns

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
+//= require dataTables/extras/dataTables.responsive
+//= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require dataTables/extras/dataTables.tableTools
 //= require jquery-ui/datepicker

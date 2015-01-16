c8fd78a75e635cee2e9d87bd855b88501a6eb576
rodrihub <saigo.getsuga@gmail.com>
2015-01-16 21:08:24 +0000

    Added redirect to 'login' in welcome page if user is not logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   skip_before_filter :check_session, :only => [:welcome]
 
   def welcome
+    redirect_to ingresar_path unless signed_in?
   end
 
 end

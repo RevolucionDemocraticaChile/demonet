3a00cf77cfaf5a8366b4d0df95dac681b71a1e3c
rodrihub <saigo.getsuga@gmail.com>
2015-01-21 17:42:44 +0000

    Fit the width of the tables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
-//= require dataTables/extras/dataTables.responsive
-//= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require dataTables/extras/dataTables.tableTools
 //= require jquery-ui/datepicker

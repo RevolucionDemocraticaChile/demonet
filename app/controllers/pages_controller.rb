dd48229a75f906f7afe9dd751573ecd339eb13c4
Gonzalo Diaz C <gdiazc@cs.ox.ac.uk>
2015-03-19 20:29:05 +0000

    bug fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   skip_authorization_check :only => [:welcome]
   skip_before_filter :check_session, :only => [:welcome]
-  # skip_before_filter :miniprofiler, :only => [:welcome]
+  skip_before_filter :miniprofiler, :only => [:welcome]
 
   def welcome
     redirect_to ingresar_path unless signed_in?
